4e3c779155752bbfd196af041b9cd361b45b8996
Mohammad Alsharif <alsharifms@gmail.com>
2025-12-26 22:03:07 +0300

    [add]: added LCD code to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+
 
 // put function declarations here:
 int myFunction(int, int);
 
+// LiquidCrystal(rs, enable, d4, d5, d6, d7)
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  lcd.begin(16, 2);
+  lcd.print("Temp : " + String(result) + " C");
+  lcd.setCursor(0, 1);
+  lcd.print("Humid: " + String(result + 10) + " %");
 }
 
 void loop() {
