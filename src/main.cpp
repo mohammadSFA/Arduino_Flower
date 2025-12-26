3e5cf138967f58bc54f77aab40baafe6df50b4f0
Mohammad Alsharif <alsharifms@gmail.com>
2025-12-26 22:25:29 +0300

    [add]: added temp/humidity sensor and code for it

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
-
+#include <DHT.h>
 
 // put function declarations here:
 int myFunction(int, int);
@@ -8,17 +8,31 @@ int myFunction(int, int);
 // LiquidCrystal(rs, enable, d4, d5, d6, d7)
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+#define DHT11_PIN 6
+DHT dht(DHT11_PIN, DHT11);
+
+// Global variables for DHT readings
+float temperature = 0;
+float humidity = 0;
+
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
   lcd.begin(16, 2);
-  lcd.print("Temp : " + String(result) + " C");
+  dht.begin();
+  lcd.print("Temp = " + String(temperature) + " C");
   lcd.setCursor(0, 1);
-  lcd.print("Humid: " + String(result + 10) + " %");
+  lcd.print("Humid= " + String(humidity) + " %");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+  lcd.setCursor(6, 0);
+  lcd.print(String(temperature));
+  lcd.setCursor(6, 1);
+  lcd.print(String(humidity));
+  delay(2000);
 }
 
 // put function definitions here:
